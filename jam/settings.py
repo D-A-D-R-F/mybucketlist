6f2368d8ed27b1040528a4b675d1c4fa53b04884
D-A-D-R-F <davis.furtado07@gmail.com>
2022-04-17 19:35:57 +0530

    Security

--- jam/settings.py
+++ jam/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#%u2g+7+9j2si9ecg4--nv_r%u(&*qsh0dkel**dc6fov5uk6*'
+SECRET_KEY = os.environ.get("secret-key-april")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
